c00528ec478077678e74ae991d3612ac862dce37
sypatrick <booth20037@gmail.com>
2023-02-06 20:42:11 +0900

    initial commit

---
+++ src/main/java/jpabasic/JpaMain.java
@@ -0,0 +1,31 @@
+package jpabasic;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] arg){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("patrick");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");
+
+            em.persist(member);
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
